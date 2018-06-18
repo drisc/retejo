936bebe78c5bce5dda2ac84772d51cd5f06aefbc
Cory Driscoll <cory@doomworks.net>
2018-06-18 10:33:15 +0100

    Added new routes to main file

--- retejo.js
+++ retejo.js
@@ -4,7 +4,7 @@ var createError = require('http-errors'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     logger = require('morgan'),
-    stylus = require('stylus');
+    stylus = require('stylus'),;
 
 var publicRouter = require('./routes/public'),
     privateRouter = require('./routes/private');
@@ -28,7 +28,8 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', publicRouter);
-app.use('/blog', publicRouter);
+app.use('/writing', publicRouter);
+app.use('/art', publicRouter);
 app.use('/about', publicRouter);
 app.use('/retejo', publicRouter);
 app.use('/login', privateRouter);
