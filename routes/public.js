bf6a2aff84c98397ca44e62c62535484eac16191
Cory Driscoll <cory@doomworks.net>
2018-07-12 21:29:28 +0100

    Mobile style changes

--- routes/public.js
+++ routes/public.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var router = express.Router();
 
 /*
@@ -6,35 +7,27 @@ var router = express.Router();
 */
 
 //  Index
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', {
     title: 'Retejo'
   });
 });
 // Art
-router.get('/art', function(req, res, next) {
+router.get('/art', function(req, res) {
   res.render('art', {
     title: 'Art'
   });
 });
 // Writing
-router.get('/writing', function(req, res, next) {
-  res.render('writing', {
-    title: 'Writing'
-  });
+router.get('/writing', function(req, res) {
+    res.render('writing', {
+        title: 'Writing',
+    });
 });
 
 // About
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
   res.render('about', {title: 'About'})
-});
-
-// Retejo
-router.get('/retejo', function(req, res, next) {
-  res.render('tidbits/post', {
-    title: 'Retejo',
-    body: 'Retejo | Website built on Node.js'
-  });
 });
 
 // Login
