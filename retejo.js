eed0ed3456bd6bd015c5ee2a94fe3bd3cf79642f
Cory Driscoll <cory@doomworks.net>
2018-07-12 19:19:46 +0100

    Merged and cleaned up generated files for server app

--- retejo.js
+++ retejo.js
@@ -1,54 +1,62 @@
-var createError = require('http-errors'),
-    express = require('express'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    logger = require('morgan'),
-    stylus = require('stylus');
+var express      = require('express'),
+    http         = require('http'),
+    path         = require('path'),
+    logger       = require('morgan'),
+    stylus       = require('stylus'),
+    httpError    = require('http-errors'),
+    errorHandler = require('errorhandler'),
+    bodyParser   = require('body-parser'),
+    cookieParser = require('cookie-parser');
 
-var publicRouter = require('./routes/public'),
+var publicRouter  = require('./routes/public'),
     privateRouter = require('./routes/private');
 
-var app = express();
+var retejo = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// App config
+retejo.set('port', process.env.PORT || 3000);
+retejo.use(logger('dev'));
+retejo.use(express.json());
+retejo.use(bodyParser.urlencoded({ extended: true }));
+retejo.use(cookieParser());
+retejo.use(stylus.middleware(path.join(__dirname, 'public')));
+retejo.use(express.static(path.join(__dirname, 'public')));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+if ('development' == retejo.get('env')) {
+    retejo.use(errorHandler());
+}
 
-app.use(function (req, res, next) {
-    res.header('X-powered-by', 'Retejo (https://drisc.io/retejo');
-  next();
+// Public Routes
+retejo.get('/', publicRouter);
+retejo.get('/writing', publicRouter);
+retejo.get('/about', publicRouter);
+
+// Private Routes
+retejo.get('/login', privateRouter);
+
+// Page Renderer
+retejo.set('views', path.join(__dirname, 'views'));
+retejo.set('view engine', 'pug');
+
+// Catch httpErrors and forward to renderer
+retejo.use(function (res, req, next) {
+    next(httpError(404));
+});
+/*
+retejo.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.retejo.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+    */
+
+
+// Start HTTP server
+http.createServer(retejo).listen(retejo.get('port'), function () {
+    console.log('Retejo server listening on port ' + retejo.get('port'));
 });
 
-app.use('/', publicRouter);
-app.use('/writing', publicRouter);
-app.use('/art', publicRouter);
-app.use('/about', publicRouter);
-app.use('/retejo', publicRouter);
-app.use('/login', privateRouter);
-app.use('/blog/new', privateRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = retejo;
